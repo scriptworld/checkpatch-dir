6e861875423d9f63d396ba0d1ca259bb682be17a
Ji-Hun Kim <jihuun.k@gmail.com>
2017-12-29 15:36:37 -0800

    Add new features
    1. Option --help
    2. Check the output with editor
    
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

--- checkpatch-dir.sh
+++ checkpatch-dir.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 
-dir_search () {
+EDITOR=vim
+SCRIPT=./scripts/checkpatch.pl
+OUTPUT=~/.checkpatch.log
 
+dir_search () {
 	for v in $(ls $1)
 	do
 		if [[ -d $1/$v ]]
@@ -10,9 +13,29 @@ dir_search () {
 			dir_search $1/$v
 		elif [[ -f $1/$v ]]
 		then
-			./scripts/checkpatch.pl -f $1/$v
+			$SCRIPT -f $1/$v
 		fi
 	done
 }
 
-dir_search $1
+if [[ $1 == '-h' || $1 == '--help' ]]
+then
+	vim -c $OPTION $OUTPUT
+	echo "Usage :
+	$ cd \$ROOT_PATH_OF_KERNEL_SOURCE
+	$ checkpatch-dir.sh \$TARGET_DIR"
+elif [[ $# -eq 0 ]]
+then
+	echo "No arguments provided"
+	exit 1
+else
+	echo "Please wait.."
+	dir_search $1 > $OUTPUT
+	cat $OUTPUT | grep "please review"
+	echo "check the file $OUTPUT : [y/n]"
+	read C
+	if [[ $C == 'y' ]]
+	then
+		$EDITOR $OUTPUT
+	fi	
+fi
