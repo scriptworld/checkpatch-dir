8fecc3148859a49b36589acd4939c78717ab3009
Ji-Hun Kim <jihuun.k@gmail.com>
2017-12-30 13:44:38 -0800

    Add feature: no needs to move root directory
    Signed-off-by: Ji-Hun Kim <jihuun.k@gmail.com>

--- checkpatch-dir.sh
+++ checkpatch-dir.sh
@@ -3,14 +3,17 @@
 EDITOR=vim
 SCRIPT=./scripts/checkpatch.pl
 OUTPUT=~/.checkpatch.log
+TARGET_DIR=$1
+GREP_STRING="please review"
 
-dir_search () {
+search_dir ()
+{
 	for v in $(ls $1)
 	do
 		if [[ -d $1/$v ]]
 		then
 			echo searching for directory $1/$v
-			dir_search $1/$v
+			search_dir $1/$v
 		elif [[ -f $1/$v ]]
 		then
 			$SCRIPT -f $1/$v
@@ -18,16 +21,32 @@ dir_search () {
 	done
 }
 
+# move for executing ./scripts/checkpatch.pl
+move_root_dir ()
+{
+	while [ ! -d kernel -o ! -d arch -o ! -d drivers ]
+	do
+		cd ..
+	done
+}
+
+make_target_dir ()
+{
+	TARGET_DIR=$(pwd)/$1
+	echo "target_dir is $TARGET_DIR"
+}
+
 case $1 in
 	-h|--help|"")
 		echo "Usage :
-		$ cd \$ROOT_PATH_OF_KERNEL_SOURCE
 		$ checkpatch-dir.sh \$TARGET_DIR"
 		;;
 	*)
+		make_target_dir $1
+		move_root_dir
 		echo "Please wait.."
-		dir_search $1 > $OUTPUT
-		cat $OUTPUT | grep "please review"
+		search_dir $TARGET_DIR > $OUTPUT
+		cat $OUTPUT | grep "$GREP_STRING"
 		echo "check the file $OUTPUT : [y/n]"
 		read C
 		if [[ $C == 'y' ]]
